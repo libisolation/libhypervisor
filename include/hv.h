dd99bd422579334f9553a2ca275e370beff9c17b
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-07-08 15:18:40 +0900

    implement more functions

--- include/hv.h
+++ include/hv.h
@@ -11,66 +11,67 @@ extern "C" {
 
 typedef unsigned vmm_return_t;
 typedef unsigned vmm_vcpuid_t;
-typedef uint64_t vmm_uvaddr_t, vmm_gpaddr_t;
+typedef const void *vmm_uvaddr_t;
+typedef uint64_t vmm_gpaddr_t;
 typedef uint64_t vmm_vmem_flags_t, vmm_vcpu_flags_t;
 
 typedef enum {
-  VMM_X86_RIP,
-  VMM_X86_RFLAGS,
-  VMM_X86_RAX,
-  VMM_X86_RCX,
-  VMM_X86_RDX,
-  VMM_X86_RBX,
-  VMM_X86_RSI,
-  VMM_X86_RDI,
-  VMM_X86_RSP,
-  VMM_X86_RBP,
-  VMM_X86_R8,
-  VMM_X86_R9,
-  VMM_X86_R10,
-  VMM_X86_R11,
-  VMM_X86_R12,
-  VMM_X86_R13,
-  VMM_X86_R14,
-  VMM_X86_R15,
-  VMM_X86_CS,
-  VMM_X86_SS,
-  VMM_X86_DS,
-  VMM_X86_ES,
-  VMM_X86_FS,
-  VMM_X86_GS,
-  VMM_X86_IDT_BASE,
-  VMM_X86_IDT_LIMIT,
-  VMM_X86_GDT_BASE,
-  VMM_X86_GDT_LIMIT,
-  VMM_X86_LDTR,
-  VMM_X86_LDT_BASE,
-  VMM_X86_LDT_LIMIT,
-  VMM_X86_LDT_AR,
-  VMM_X86_TR,
-  VMM_X86_TSS_BASE,
-  VMM_X86_TSS_LIMIT,
-  VMM_X86_TSS_AR,
-  VMM_X86_CR0,
-  VMM_X86_CR1,
-  VMM_X86_CR2,
-  VMM_X86_CR3,
-  VMM_X86_CR4,
-  VMM_X86_DR0,
-  VMM_X86_DR1,
-  VMM_X86_DR2,
-  VMM_X86_DR3,
-  VMM_X86_DR4,
-  VMM_X86_DR5,
-  VMM_X86_DR6,
-  VMM_X86_DR7,
-  VMM_X86_TPR,
-  VMM_X86_XCR0,
-  VMM_X86_REGISTERS_MAX,
-} vmm_x86_reg_t;
+  VMM_X64_RIP,
+  VMM_X64_RFLAGS,
+  VMM_X64_RAX,
+  VMM_X64_RCX,
+  VMM_X64_RDX,
+  VMM_X64_RBX,
+  VMM_X64_RSI,
+  VMM_X64_RDI,
+  VMM_X64_RSP,
+  VMM_X64_RBP,
+  VMM_X64_R8,
+  VMM_X64_R9,
+  VMM_X64_R10,
+  VMM_X64_R11,
+  VMM_X64_R12,
+  VMM_X64_R13,
+  VMM_X64_R14,
+  VMM_X64_R15,
+  VMM_X64_CS,
+  VMM_X64_SS,
+  VMM_X64_DS,
+  VMM_X64_ES,
+  VMM_X64_FS,
+  VMM_X64_GS,
+  VMM_X64_IDT_BASE,
+  VMM_X64_IDT_LIMIT,
+  VMM_X64_GDT_BASE,
+  VMM_X64_GDT_LIMIT,
+  VMM_X64_LDTR,
+  VMM_X64_LDT_BASE,
+  VMM_X64_LDT_LIMIT,
+  VMM_X64_LDT_AR,
+  VMM_X64_TR,
+  VMM_X64_TSS_BASE,
+  VMM_X64_TSS_LIMIT,
+  VMM_X64_TSS_AR,
+  VMM_X64_CR0,
+  VMM_X64_CR1,
+  VMM_X64_CR2,
+  VMM_X64_CR3,
+  VMM_X64_CR4,
+  VMM_X64_DR0,
+  VMM_X64_DR1,
+  VMM_X64_DR2,
+  VMM_X64_DR3,
+  VMM_X64_DR4,
+  VMM_X64_DR5,
+  VMM_X64_DR6,
+  VMM_X64_DR7,
+  VMM_X64_TPR,
+  VMM_X64_XCR0,
+  VMM_X64_REGISTERS_MAX,
+} vmm_x64_reg_t;
 
-vmm_return_t vmm_vm_create(void);
-vmm_return_t vmm_vm_destroy(void);
+vmm_return_t vmm_create(void);
+vmm_return_t vmm_destroy(void);
 
 vmm_return_t vmm_vmem_map(vmm_uvaddr_t uva, vmm_gpaddr_t gpa, size_t size, vmm_vmem_flags_t flags);
 vmm_return_t vmm_vmem_unmap(vmm_gpaddr_t gpa, size_t size);
@@ -78,17 +79,13 @@ vmm_return_t vmm_vmem_protect(vmm_gpaddr_t gpa, size_t size, vmm_vmem_flags_t fl
 
 vmm_return_t vmm_vcpu_create(vmm_vcpuid_t *vcpu);
 vmm_return_t vmm_vcpu_destroy(vmm_vcpuid_t vcpu);
-
-vmm_return_t vmm_vcpu_read_register(vmm_vcpuid_t vcpu, vmm_x86_reg_t reg, uint64_t *value);
-vmm_return_t vmm_vcpu_write_register(vmm_vcpuid_t vcpu, vmm_x86_reg_t reg, uint64_t value);
-
-vmm_return_t vmm_vcpu_enable_native_msr(vmm_vcpuid_t vcpu, uint32_t msr, bool enable);
-vmm_return_t vmm_vcpu_read_msr(vmm_vcpuid_t vcpu, uint32_t msr, uint64_t *value);
-vmm_return_t vmm_vcpu_write_msr(vmm_vcpuid_t vcpu, uint32_t msr, uint64_t value);
-
-vmm_return_t vmm_vcpu_invalidate_tlb(vmm_vcpuid_t vcpu);
 vmm_return_t vmm_vcpu_run(vmm_vcpuid_t vcpu);
 vmm_return_t vmm_vcpu_interrupt(vmm_vcpuid_t* vcpus, unsigned int vcpu_count);
+vmm_return_t vmm_vcpu_invalidate_tlb(vmm_vcpuid_t vcpu);
+vmm_return_t vmm_vcpu_read_register(vmm_vcpuid_t vcpu, vmm_x64_reg_t reg, uint64_t *value);
+vmm_return_t vmm_vcpu_write_register(vmm_vcpuid_t vcpu, vmm_x64_reg_t reg, uint64_t value);
+vmm_return_t vmm_vcpu_read_msr(vmm_vcpuid_t vcpu, uint32_t msr, uint64_t *value);
+vmm_return_t vmm_vcpu_write_msr(vmm_vcpuid_t vcpu, uint32_t msr, uint64_t value);
 
 #ifdef __cplusplus
 }
--- src/hv.c
+++ src/hv.c
@@ -4,3 +4,106 @@
 vmm_return_t vmm_create(void) {
   return hv_vm_create(HV_VM_DEFAULT);
 }
+
+vmm_return_t vmm_destroy(void) {
+  return hv_vm_destroy();
+}
+
+vmm_return_t vmm_vmem_map(vmm_uvaddr_t uva, vmm_gpaddr_t gpa, size_t size, vmm_vmem_flags_t flags) {
+  return hv_vm_map(uva, gpa, size, flags);
+}
+
+vmm_return_t vmm_vmem_unmap(vmm_gpaddr_t gpa, size_t size) {
+  return hv_vm_unmap(gpa, size);
+}
+
+vmm_return_t vmm_vmem_protect(vmm_gpaddr_t gpa, size_t size, vmm_vmem_flags_t flags) {
+  return hv_vm_protect(gpa, size, flags);
+}
+
+vmm_return_t vmm_vcpu_create(vmm_vcpuid_t *vcpu) {
+  return hv_vcpu_create(vcpu, HV_VCPU_DEFAULT);
+}
+
+vmm_return_t vmm_vcpu_destroy(vmm_vcpuid_t vcpu) {
+  return hv_vcpu_destroy(vcpu);
+}
+
+vmm_return_t vmm_vcpu_run(vmm_vcpuid_t vcpu) {
+  return hv_vcpu_run(vcpu);
+}
+
+static hv_x86_reg_t tr_reg(vmm_x64_reg_t reg) {
+  switch (reg) {
+#define CASE(NAME) case VMM_X64_##NAME: return HV_X86_##NAME; 
+   CASE(RIP)
+   CASE(RFLAGS)
+   CASE(RAX)
+   CASE(RCX)
+   CASE(RDX)
+   CASE(RBX)
+   CASE(RSI)
+   CASE(RDI)
+   CASE(RSP)
+   CASE(RBP)
+   CASE(R8)
+   CASE(R9)
+   CASE(R10)
+   CASE(R11)
+   CASE(R12)
+   CASE(R13)
+   CASE(R14)
+   CASE(R15)
+   CASE(CS)
+   CASE(SS)
+   CASE(DS)
+   CASE(ES)
+   CASE(FS)
+   CASE(GS)
+   CASE(IDT_BASE)
+   CASE(IDT_LIMIT)
+   CASE(GDT_BASE)
+   CASE(GDT_LIMIT)
+   CASE(LDTR)
+   CASE(LDT_BASE)
+   CASE(LDT_LIMIT)
+   CASE(LDT_AR)
+   CASE(TR)
+   CASE(TSS_BASE)
+   CASE(TSS_LIMIT)
+   CASE(TSS_AR)
+   CASE(CR0)
+   CASE(CR1)
+   CASE(CR2)
+   CASE(CR3)
+   CASE(CR4)
+   CASE(DR0)
+   CASE(DR1)
+   CASE(DR2)
+   CASE(DR3)
+   CASE(DR4)
+   CASE(DR5)
+   CASE(DR6)
+   CASE(DR7)
+   CASE(TPR)
+   CASE(XCR0)
+   CASE(REGISTERS_MAX)
+  }
+}
+
+vmm_return_t vmm_vcpu_read_register(vmm_vcpuid_t vcpu, vmm_x64_reg_t reg, uint64_t *value) {
+  return hv_vcpu_read_register(vcpu, tr_reg(reg), value);
+}
+
+vmm_return_t vmm_vcpu_write_register(vmm_vcpuid_t vcpu, vmm_x64_reg_t reg, uint64_t value) {
+  return hv_vcpu_write_register(vcpu, tr_reg(reg), value);
+}
+
+vmm_return_t vmm_vcpu_read_msr(vmm_vcpuid_t vcpu, uint32_t msr, uint64_t *value) {
+  return hv_vcpu_read_msr(vcpu, msr, value);
+}
+
+vmm_return_t vmm_vcpu_write_msr(vmm_vcpuid_t vcpu, uint32_t msr, uint64_t value) {
+  return hv_vcpu_write_msr(vcpu, msr, value);
+}
+
